af75544fe8f509954378bd80131eade82a71b4f6
slovke024 <76702780+slovke024@users.noreply.github.com>
2023-01-12 15:39:29 +0100

    Client menu
    Client kad se poveze dobije meni sa opcijama

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,21 +11,65 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            NetTcpBinding binding = new NetTcpBinding();
+            var binding = new NetTcpBinding();
             string address = "net.tcp://localhost:9999/Server";
+            var factory = new ChannelFactory<IEchoService>(binding, address);
+            var channel = factory.CreateChannel();
 
-            ChannelFactory<IEchoService> factory = new ChannelFactory<IEchoService>(binding, address);
-            IEchoService channel = factory.CreateChannel();
+            while (true)
+            {
+                ShowMenu();
+                var option = Console.ReadLine();
+                if (option == "8")
+                    break;
 
-            Console.WriteLine(channel.Echo("Hello, World!"));
-
-            Console.WriteLine("Press enter to exit.");
-            Console.ReadLine();
-
+                switch (option)
+                {
+                    case "1":
+                        // Show folder content
+                        break;
+                    case "2":
+                        // Read file
+                        break;
+                    case "3":
+                        // Create folder
+                        break;
+                    case "4":
+                        // Create file
+                        break;
+                    case "5":
+                        // Delete
+                        break;
+                    case "6":
+                        // Rename
+                        break;
+                    case "7":
+                        // Move to
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
             factory.Close();
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Show folder content");
+            Console.WriteLine("2. Read file");
+            Console.WriteLine("3. Create folder");
+            Console.WriteLine("4. Create file");
+            Console.WriteLine("5. Delete");
+            Console.WriteLine("6. Rename");
+            Console.WriteLine("7. Move to");
+            Console.WriteLine("8. Exit");
         }
     }
 }
+
+
 
 [ServiceContract]
 interface IEchoService
